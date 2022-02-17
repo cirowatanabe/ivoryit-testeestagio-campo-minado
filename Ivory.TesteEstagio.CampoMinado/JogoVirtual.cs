86ed86b0df6aaee4cec583fb1debb0e9923a73ba
Ciro Watanabe <cw.artiga@gmail.com>
2022-02-17 15:07:55 -0300

    Solução Campo Minado

---
+++ Ivory.TesteEstagio.CampoMinado/JogoVirtual.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Ivory.TesteEstagio.CampoMinado
+{
+    class JogoVirtual
+    {
+        public static void AtualizarJogo(string[,] Jogo, CampoMinado campoMinado)
+        {
+            // Esse método é responsável por criar uma matriz idêntica à matriz Jogo da classe CampoMinado. Isso é feito porque a matriz Jogo da classe CampoMinado é private e, portanto, não permite edição pelo Program.cs. No entanto, as minas precisam ser marcadas para que o programa saiba quais elementos são seguros de abrir. Essa marcação é feita na cópia gerada por este método.
+
+            string[] sequence = Regex.Split(campoMinado.Tabuleiro, string.Empty);
+            List<string> list = new List<string>();
+
+            foreach (string s in sequence)
+            {
+                if (s != "\n" && s != "\r" && s != "")
+                {
+                    list.Add(s);
+                }
+            }
+
+            for (int a = 0; a < 9; a++)
+            {
+                Jogo[0, a] = list[a];
+            }
+            for (int b = 9; b < 18; b++)
+            {
+                Jogo[1, b - 9] = list[b];
+            }
+            for (int c = 18; c < 27; c++)
+            {
+                Jogo[2, c - 18] = list[c];
+            }
+            for (int d = 27; d < 36; d++)
+            {
+                Jogo[3, d - 27] = list[d];
+            }
+            for (int e = 36; e < 45; e++)
+            {
+                Jogo[4, e - 36] = list[e];
+            }
+            for (int f = 45; f < 54; f++)
+            {
+                Jogo[5, f - 45] = list[f];
+            }
+            for (int g = 54; g < 63; g++)
+            {
+                Jogo[6, g - 54] = list[g];
+            }
+            for (int h = 63; h < 72; h++)
+            {
+                Jogo[7, h - 63] = list[h];
+            }
+            for (int i = 72; i < 81; i++)
+            {
+                Jogo[8, i - 72] = list[i];
+            }
+        }
+    }
+}
--- Ivory.TesteEstagio.CampoMinado/Program.cs
+++ Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ivory.TesteEstagio.CampoMinado
 {
@@ -12,6 +13,295 @@ namespace Ivory.TesteEstagio.CampoMinado
 
             // Realize sua codificação a partir deste ponto, boa sorte!
 
+            // Primeiro, eu preciso acessar a matriz do jogo. Como a classe Jogo é private, eu a recrio a partir da string Tabuleiro.
+
+            string[,] Jogo = new string[9, 9];
+
+            JogoVirtual.AtualizarJogo(Jogo, campoMinado);
+
+            while (true)
+            {
+                JogoVirtual.AtualizarJogo(Jogo, campoMinado);
+                for (int x = 0; x < 9; x++)
+                {
+                    for (int y = 0; y < 9; y++)
+                    {
+                        string sup = "";
+                        string inf = "";
+                        string esq = "";
+                        string dir = "";
+                        string supEsq = "";
+                        string supDir = "";
+                        string infEsq = "";
+                        string infDir = "";
+                        List<string> entorno = new List<string>();
+
+                        if (x > 0)
+                        {
+                            sup = Jogo[x - 1, y];
+                        }
+                        if (y > 0)
+                        {
+                            esq = Jogo[x, y - 1];
+                        }
+                        if (y < 8)
+                        {
+                            dir = Jogo[x, y + 1];
+                        }
+                        if (x > 0 && y > 0)
+                        {
+                            supEsq = Jogo[x - 1, y - 1];
+                        }
+                        if (x > 0 && y < 8)
+                        {
+                            supDir = Jogo[x - 1, y + 1];
+                        }
+                        if (x < 8)
+                        {
+                            inf = Jogo[x + 1, y];
+                        }
+                        if (x < 8 && y > 0)
+                        {
+                            infEsq = Jogo[x + 1, y - 1];
+                        }
+                        if (x < 8 && y < 8)
+                        {
+                            infDir = Jogo[x + 1, y + 1];
+                        }
+
+                        if (Jogo[x, y] != "-" && Jogo[x, y] != "0" && Jogo[x, y] != "M")
+                        {
+                            int counter = 0;
+                            int counterMines = 0;
+
+                            // 0
+                            entorno.Add(sup);
+                            // 1
+                            entorno.Add(inf);
+                            // 2
+                            entorno.Add(esq);
+                            // 3
+                            entorno.Add(dir);
+                            // 4
+                            entorno.Add(supEsq);
+                            // 5
+                            entorno.Add(supDir);
+                            // 6
+                            entorno.Add(infEsq);
+                            // 7
+                            entorno.Add(infDir);
+
+                            foreach (string item in entorno)
+                            {
+                                if (item == "-")
+                                {
+                                    counter++;
+                                }
+                                else if (item == "M")
+                                {
+                                    counterMines++;
+                                }
+                            }
+
+                            // Marcação das minas
+
+                            if (int.Parse(Jogo[x, y]) - counterMines == counter)
+                            {
+                                List<int> posicoesMinas = new List<int>();
+                                for (int i = 0; i < entorno.Count; i++)
+                                {
+                                    if (entorno[i] == "-")
+                                    {
+                                        posicoesMinas.Add(i);
+                                        counterMines++;
+                                    }
+                                }
+
+                                // E.g.: se "-" estiver na posição 0 da lista 'entorno', então a mina estará em cima (sup) do número analisado
+
+                                foreach (int pos in posicoesMinas)
+                                {
+                                    if (pos == 0)
+                                    {
+                                        Jogo[x - 1, y] = "M";
+                                    }
+                                    else if (pos == 1)
+                                    {
+                                        Jogo[x + 1, y] = "M";
+                                    }
+                                    else if (pos == 2)
+                                    {
+                                        Jogo[x, y - 1] = "M";
+                                    }
+                                    else if (pos == 3)
+                                    {
+                                        Jogo[x, y + 1] = "M";
+                                    }
+                                    else if (pos == 4)
+                                    {
+                                        Jogo[x - 1, y - 1] = "M";
+                                    }
+                                    else if (pos == 5)
+                                    {
+                                        Jogo[x - 1, y + 1] = "M";
+                                    }
+                                    else if (pos == 6)
+                                    {
+                                        Jogo[x + 1, y - 1] = "M";
+                                    }
+                                    else if (pos == 7)
+                                    {
+                                        Jogo[x + 1, y + 1] = "M";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                for (int x = 0; x < 9; x++)
+                {
+                    for (int y = 0; y < 9; y++)
+                    {
+                        string sup = "";
+                        string inf = "";
+                        string esq = "";
+                        string dir = "";
+                        string supEsq = "";
+                        string supDir = "";
+                        string infEsq = "";
+                        string infDir = "";
+                        List<string> entorno = new List<string>();
+                        if (x > 0)
+                        {
+                            sup = Jogo[x - 1, y];
+                        }
+                        if (y > 0)
+                        {
+                            esq = Jogo[x, y - 1];
+                        }
+                        if (y < 8)
+                        {
+                            dir = Jogo[x, y + 1];
+                        }
+                        if (x > 0 && y > 0)
+                        {
+                            supEsq = Jogo[x - 1, y - 1];
+                        }
+                        if (x > 0 && y < 8)
+                        {
+                            supDir = Jogo[x - 1, y + 1];
+                        }
+                        if (x < 8)
+                        {
+                            inf = Jogo[x + 1, y];
+                        }
+                        if (x < 8 && y > 0)
+                        {
+                            infEsq = Jogo[x + 1, y - 1];
+                        }
+                        if (x < 8 && y < 8)
+                        {
+                            infDir = Jogo[x + 1, y + 1];
+                        }
+
+                        if (Jogo[x, y] != "-" && Jogo[x, y] != "0" && Jogo[x, y] != "M")
+                        {
+                            int counter = 0;
+                            int counterMines = 0;
+
+                            // 0
+                            entorno.Add(sup);
+                            // 1
+                            entorno.Add(inf);
+                            // 2
+                            entorno.Add(esq);
+                            // 3
+                            entorno.Add(dir);
+                            // 4
+                            entorno.Add(supEsq);
+                            // 5
+                            entorno.Add(supDir);
+                            // 6
+                            entorno.Add(infEsq);
+                            // 7
+                            entorno.Add(infDir);
+
+                            foreach (string item in entorno)
+                            {
+                                if (item == "-")
+                                {
+                                    counter++;
+                                }
+                                else if (item == "M")
+                                {
+                                    counterMines++;
+                                }
+                            }
+
+                            // Caso o número de minas seja igual ao número analisado e ainda houver "-" no entorno do número:
+
+                            if (counterMines == int.Parse(Jogo[x, y]) && counter != 0)
+                            {
+                                List<int> posicoesRiscos = new List<int>();
+                                for (int i = 0; i < entorno.Count; i++)
+                                {
+                                    if (entorno[i] == "-")
+                                    {
+                                        posicoesRiscos.Add(i);
+                                    }
+                                }
+                                foreach (int pos in posicoesRiscos)
+                                {
+                                    if (pos == 0)
+                                    {
+                                        campoMinado.Abrir(x, y + 1);
+                                    }
+                                    if (pos == 1)
+                                    {
+                                        campoMinado.Abrir(x + 2, y + 1);
+                                    }
+                                    if (pos == 2)
+                                    {
+                                        campoMinado.Abrir(x + 1, y);
+                                    }
+                                    if (pos == 3)
+                                    {
+                                        campoMinado.Abrir(x + 1, y + 2);
+                                    }
+                                    if (pos == 4)
+                                    {
+                                        campoMinado.Abrir(x, y);
+                                    }
+                                    if (pos == 5)
+                                    {
+                                        campoMinado.Abrir(x, y + 2);
+                                    }
+                                    if (pos == 6)
+                                    {
+                                        campoMinado.Abrir(x + 2, y);
+                                    }
+                                    if (pos == 7)
+                                    {
+                                        campoMinado.Abrir(x + 2, y + 2);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                if (campoMinado.JogoStatus != 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=========");
+            Console.WriteLine();
+            Console.WriteLine(campoMinado.Tabuleiro);
+            Console.WriteLine();
+            Console.WriteLine($"STATUS DO JOGO: {campoMinado.JogoStatus}");
         }
     }
 }
